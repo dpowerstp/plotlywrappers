76d6bb1d5890c81b4fef00472bc1064926f6aa53
dpowerstp <danielp@takomaparkmd.gov>
2022-04-27 11:22:33 -0400

    addded multi_lines function and notitlesaxes function; added examples

--- R/dotted_line.R
+++ R/dotted_line.R
@@ -1,6 +1,8 @@
 #' Dotted line
 #' Adds dotted line with label to plot_ly plot
 #'
+#' @importFrom magrittr %>%
+#'
 #' @param plot plot_ly plot
 #' @param df Dataframe used to build plot_ly plot
 #' @param x_col String column name of x-axis
@@ -14,7 +16,16 @@
 #' @return plot_ly plot with dotted line and label on dotted line
 #' @export
 #'
-#' @examples
+#' @examples{
+#'
+#' quickplot <- plotly::plot_ly(mtcars,
+#' x = ~ carb,
+#' y = ~ disp,
+#' type = "bar")
+#'
+#' dotted_line(quickplot, mtcars, "carb", 200, "200 line")
+#'
+#' }
 dotted_line <- function(plot, df, x_col, line_val, gluetext, .yshift = 5, .xshift = 2, .showarrow = T, .lineadjust = 0.5){
   x_length <- length(df[[x_col]] %>%
                        unique())
@@ -42,4 +53,61 @@ dotted_line <- function(plot, df, x_col, line_val, gluetext, .yshift = 5, .xshif
       xshift = .xshift,
       y = line_val,
       showarrow = .showarrow)
+}
+
+
+
+
+#' Multiple Dotted Lines
+#'
+#' Create multiple dotted lines in a plot_ly graph
+#'
+#' @param plot Base-plotly chart
+#' @param df Base-dataframe
+#' @param x_col String name of x-axis column
+#' @param yvec Numeric vector of y-values at which to draw dotted lines
+#' @param textvec Character vector of labels for dotted lines; fed to glue function
+#' @param .lineadjust How much to adjust the length of the dotted line relative to the x-axis; default 0.5
+#' @param .showarrow Whether to show an arrow pointing to the dotted line or not; default T
+#' @param .yshift How much to adjust the y-axis position of the text label relative to the dotted line; default 5
+#' @param .xshift How much to adjust the x-axis position of the text label relative to the dotted line; default 2
+#'
+#' @return
+#' @export
+#'
+#' @examples{
+#'
+#' quickplot <- plotly::plot_ly(mtcars,
+#' x = ~ carb,
+#' y = ~ disp,
+#' type = "bar")
+#'
+#' multi_line(quickplot, mtcars, "carb", c(200, 250), c("200 line", "250 line"))
+#'
+#' }
+multi_line <- function(plot, df, x_col, yvec, textvec, .lineadjust = 0.5, .showarrow =T, .yshift = 5, .xshift = 2){
+
+  if (length(yvec) != length(textvec)){
+    stop("Length of yvec must equal length of textvec")
+  }
+
+  x_length <- length(df[[x_col]] %>% unique())
+
+  linelist <- purrr::map(yvec, ~{
+    list(type = "line",
+         y0 = .x, y1 = .x, x0 = 0 - .lineadjust, x1 = x_length -
+           .lineadjust, xref = "x", line = list(dash = "dash"))
+  })
+
+  purrr::walk2(textvec, yvec, ~ {
+
+    plot <<- plot %>%
+      plotly::add_annotations(text = glue::glue(.x),
+                              x = x_length - x_length/2, yshift = .yshift, xshift = .xshift,
+                              y = .y, showarrow = .showarrow)
+  })
+
+  plot %>%
+    plotly::layout("shapes" = linelist)
+
 }
--- R/subplot_title.R
+++ R/subplot_title.R
@@ -1,5 +1,6 @@
 #' Function to add a subplot title to a plotly subplot
 #'
+#'
 #' @param plot plotly subplot
 #' @param title title of subplot
 #' @param .x Numeric position of subplot title relative to x-axis
@@ -8,7 +9,16 @@
 #' @return plotly with subtitle
 #' @export
 #'
-#' @examples
+#' @examples{
+#'
+#' quickplot <- plotly::plot_ly(mtcars,
+#' x = ~ carb,
+#' y = ~ disp,
+#' type = "bar")
+#'
+#' subplot_title(quickplot, "Cars subplot")
+#'
+#' }
 subplot_title <- function(plot, title, .x = 0.1, .y = 1){
 
   plot %>%
@@ -21,4 +31,33 @@ subplot_title <- function(plot, title, .x = 0.1, .y = 1){
                             yanchor = "top",
                             showarrow = FALSE,
                             font = list(size = 12))
+}
+
+
+
+
+#' Remove Plot and Axes Titles
+#'
+#' Set title of plotly plot to nothing, and title of axes to nothing. Useful if creating subplots and want to set layout at end.
+#'
+#' @param plot Plotly plot
+#'
+#' @return Plotly plot with no labels.
+#' @export
+#'
+#' @examples{
+#'
+#' quickplot <- plotly::plot_ly(mtcars,
+#' x = ~ carb,
+#' y = ~ disp,
+#' type = "bar")
+#'
+#' no_titleaxeslabels(quickplot)
+#'
+#' }
+no_titleaxeslabels <- function(plot){
+  plot %>%
+    plotly::layout(xaxis = list(title = ""),
+           yaxis = list(title = ""),
+           title = "")
 }
